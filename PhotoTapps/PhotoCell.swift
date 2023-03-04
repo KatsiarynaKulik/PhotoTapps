87e52852bc25a611d73ff2163995f39b1505f645
Katsiaryna Kulik <kulik_03_10@icloud.com>
2023-03-04 14:53:10 +0400

    Add flow Layout Advanced Setup, Custom CollectionView Cell

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Екатерина  on 2.03.23.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+    
+    
+}
